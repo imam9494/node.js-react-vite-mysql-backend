099f818c2f397ce6340f8b0e70e1679645e0db4e
imam <muhammad.imamudin@gmail.com>
2026-07-07 19:52:41 +0700

    Use environment variables for MySQL configuration

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "appuser",
-  password: "app123",
-  database: "auth_app",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool.promise();
